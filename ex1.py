bac1bda9215df1f1c7e34a727103e0a90604002c
Stuffing <stuffingeu56@gmail.com>
2020-10-19 15:48:43 +0300

    conspect update

--- ex1.py
+++ ex1.py
@@ -1,16 +1,32 @@
-class Point3D:
-    coords = []
+class Point:
 
-    def __init__(self, args):
-        self.coords = args
+    def __init__(self, x=0, y=0):
+        self.__x = x
+        self.__y = y
 
-    def get_coords(self):
-        return tuple(self.coords)
+    def __checkVaue(x):
+        if isinstance(x, int) or isinstance(x, float):
+            return True
+        return False
 
-    def set_coords(self, pos, val):
-        self.coords[pos] = val
+    def __setCoordX(self, x):
+        if Point.__checkVaue(x):
+            self.__x = x
+        else:
+            raise ValueError("Неверный формат данных")
 
+    def __getCoordX(self):
+        return self.__x
 
-pt = Point3D([1, 2, 3, 4])
-pt.set_coords(2, 5)
-print(pt.get_coords())
+    def __delCoordX(self):
+        print("Удаление свойства")
+        del self.__x
+
+    coordX = property(__getCoordX, __setCoordX, __delCoordX)
+pt = Point(1, 2)
+pt.coordX = 100
+x = pt.coordX
+print(x)
+del pt.coordX
+pt.coordX = 7
+pt.coordX
--- lesson.py
+++ lesson.py
@@ -1,7 +1,3 @@
-class Car:
-    model = "BMW"
-    engine = 1.6
-
-    @staticmethod
-    def drive():
-        print("Let's go")
+a = -1
+a = -a
+print(a)
