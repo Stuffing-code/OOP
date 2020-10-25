e53e4270dad628d247c2b103f1783d458eaa4ddb
Stuffing <stuffingeu56@gmail.com>
2020-10-25 17:18:37 +0300

    записал тз

--- lesson.py
+++ lesson.py
@@ -6,74 +6,32 @@ class Point:
     def __str__(self):
         return f"({self.__x}, {self.__y})"
 
-    def isDigit(self):
-        if (isinstance(self.__x, int) or isinstance(self.__x, float)) and (
-                isinstance(self.__y, int) or isinstance(self.__y, float)):
-            return True
-        return False
 
-    def isInt(self):
-        if isinstance(self.__x, int) and isinstance(self.__y, int):
-            return True
-        return False
+class Styles:
+    def __init__(self):
+        print("Constr Style")
+        super().__init__()
 
 
-class Prop:
-    def __init__(self, sp: Point, ep: Point, color: str = "red", width: int = 1):
+class Pos:
+    def __init__(self):
+        print("Constr Pos")
+        super().__init__()
+
+
+class Line(Pos, Styles):
+    def __init__(self, sp: Point, ep: Point, color = "red", width = 1):
         self._sp = sp
         self._ep = ep
         self._color = color
-        self.__width = width
-
-    def getWidth(self):
-        return self.__width
-
-    def setCoords(self, sp, ep):
-        if sp.isDigit() and ep.isDigit():
-            self._sp = sp
-            self._ep = ep
+        self._width = width
+        super().__init__()
 
     def draw(self):
-        raise NotImplementedError("Необходим метод draw()")
-
-class Line(Prop):
-    def draw(self):
-        print(f"Рисование линии: {self._sp}, {self._ep}, {self._color}, {self.getWidth()}")
-    #
-    # def __setOneCoord(self, sp):
-    #     if sp.isInt():
-    #         self._sp = sp
-    #     else:
-    #         print("Координаты должны быть целочислеными")
-    #
-    # def __setTwoCoords(self, sp, ep):
-    #     if sp.isInt() and ep.isInt():
-    #         Prop.setCoords(self, sp, ep)
-    #     else:
-    #         print("Координаты должны быть целочислеными")
-    #
-    # def setCoords(self, sp: Point, ep: Point = None):
-    #     if ep is None:
-    #         self.__setOneCoord(sp)
-    #     else:
-    #         self.__setTwoCoords(sp, ep)
+        print(f"Рисование линии: {self._sp}, {self._ep}, {self._color}, {self._width}")
 
 
-class Rect(Prop):
-    def draw(self):
-        print(f"Рисование прямоугольника: {self._sp}, {self._ep}, {self._color}, {self.getWidth()}")
+l = Line(Point(10, 10), Point(100, 100), "green", 5)
+l.draw()
 
-
-class Ellipse(Prop):
-    def draw(self):
-        print(f"Рисование элипса: {self._sp}, {self._ep}, {self._color}, {self.getWidth()}")
-
-
-figs = []
-figs.append(Line(Point(0, 0), Point(10, 10)))
-figs.append(Line(Point(10, 10), Point(20, 10)))
-figs.append(Rect(Point(50, 50), Point(100, 100)))
-figs.append(Ellipse(Point(-10, -10), Point(10, 10)))
-
-for f in figs:
-    f.draw()
+print(Line.__mro__)
