fbd30ed7523c7f941689385136ee226cf03639af
Stuffing <stuffingeu56@gmail.com>
2020-10-26 09:03:59 +0300

    записал тз

--- dz9.py
+++ dz9.py
@@ -1,6 +1,13 @@
 """
 Создайте дочерний класс Motherboard, которая наследуеться
 отк класов: CPU, GPU, Memory. В свою очередь CPU
-наследуется от классов: AMD и Intell, GPU от классов
-NVidia, GeForce.
+наследуется от классов: AMD и Intel, GPU от классов
+Nvidia, GeForce.
 """
+
+class AMD:
+    pass
+
+
+class Intel:
+    pass
